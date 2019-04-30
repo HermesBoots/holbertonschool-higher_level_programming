6ba4423a381dcd096df95412e0956f2fc95246d7
HermesBoots <samuel.of.hermes@gmail.com>
2019-04-30 17:15:34 +0000

    Remove string join and generator from fifteenth task

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-r1 = (122, 96, -2)
-r2 = (89, 64, -2)
-print(''.join(chr(a)+chr(b) for a, b in zip(range(*r1), range(*r2))), end='')
+index = 25
+for little, big in zip(range(122, 96, -1), range(90, 64, -1)):
+    print('{:c}'.format(little if index % 2 else big), end='')
+    index -= 1
