700aad0b2903e27d868711c3195f3d8742b90188
HermesBoots <samuel.of.hermes@gmail.com>
2019-04-30 16:53:24 +0000

    Simplify seventh task to look like C solution

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,2 +1,7 @@
 #!/usr/bin/python3
-print(', '.join(str(a) + str(b) for a in range(10) for b in range(10) if b > a))
+for first in range(10):
+    for second in range(first + 1, 10):
+        print('{:d}{:d}'.format(first, second), end='')
+        if first != 8 or second != 9:
+            print(', ', end='')
+print()
