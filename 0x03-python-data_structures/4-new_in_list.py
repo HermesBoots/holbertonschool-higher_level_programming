5094c93f19018724e7b478d3d022863f60d01eec
HermesBoots <samuel.of.hermes@gmail.com>
2019-05-07 16:22:18 +0000

    Fix style of Python files

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-	l = list(my_list)
-	if idx >= 0 and idx <= len(l):
-		l[idx] = element
-	return l
+    copy = list(my_list)
+    if idx >= 0 and idx <= len(copy):
+        copy[idx] = element
+    return copy
--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-	if len(my_list) != 0:
-		m = float('-inf')
-		for i in my_list:
-			if i > m:
-				m = i
-		return m
+    if len(my_list) != 0:
+        m = float('-inf')
+        for i in my_list:
+            if i > m:
+                m = i
+        return m
