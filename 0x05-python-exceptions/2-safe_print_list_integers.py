50c12381e82ae08fcea8d2677d168a6339403900
HermesBoots <samuel.of.hermes@gmail.com>
2019-05-20 15:12:16 +0000

    Add third task: print only integers from list

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    count, index = 0, 0
+    while index < x:
+        try:
+            print("{:d}".format(my_list[index]), end="")
+        except (TypeError, ValueError):
+            pass
+        else:
+            count += 1
+        finally:
+            index += 1
+    print()
+    return count
--- 0x05-python-exceptions/README.md
+++ 0x05-python-exceptions/README.md
@@ -5,3 +5,6 @@ Printing a number of items in a list that may exceed the list's size without exc
 
 ## Safe Printing of an Integers List
 Printing an integer and handling exceptions if it isn't one.
+
+## Print and Count Integers
+Printing integers in a list and handling exceptions if an item isn't a number or there aren't enough items to print.
