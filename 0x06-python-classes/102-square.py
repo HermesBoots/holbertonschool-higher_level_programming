35502df120b2fe0b2fe47d3680a201ef84ce8131
HermesBoots <samuel.of.hermes@gmail.com>
2019-05-21 22:57:45 +0000

    Add check for negative position in ninth task

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -29,6 +29,8 @@ class Square:
             raise TypeError('position must be a tuple of 2 positive integers')
         if not isinstance(val[0], int) or not isinstance(val[1], int):
             raise TypeError('position must be a tuple of 2 positive integers')
+        if val[0] < 0 or val[1] < 0:
+            raise TypeError('position must be a tuple of 2 positive integers')
         self.__position = val
 
     @property
