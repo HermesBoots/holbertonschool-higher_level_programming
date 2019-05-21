a733be6ffacb319b1647b955643010e13488d348
HermesBoots <samuel.of.hermes@gmail.com>
2019-05-21 23:11:30 +0000

    Add unused default value to eleventh task

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -5,7 +5,7 @@ import math
 
 class MagicClass:
     """class to recreate Python byte code"""
-    def __init__(self, radius):
+    def __init__(self, radius=0):
         """create new Circle object
 
         Args:
