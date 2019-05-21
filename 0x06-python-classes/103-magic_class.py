78aa2f464d945a4eae3db157a47f9d01074887ee
HermesBoots <samuel.of.hermes@gmail.com>
2019-05-21 22:22:32 +0000

    Add extra docstring to eleventh task

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
+"""Python source code that produces exact byte code"""
 import math
 
 
 class MagicClass:
     """class to recreate Python byte code"""
-
     def __init__(self, radius):
         """create new Circle object"""
         self.__radius = 0
