142196c46b36019783ad6ec038272212456290c7
HermesBoots <samuel.of.hermes@gmail.com>
2019-05-21 14:47:11 +0000

    Add third task: validate constructor parameter

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""The Square class
+
+Definition of a class that describes a square
+
+"""
+
+
+class Square:
+    """A geometric square
+
+    Contains information about a square and methods for maniuplating it
+
+    """
+
+    def __init__(self, size=0):
+        """Create a square with a given size
+
+        The size of the new square instance is hidden, to be accessed later.
+
+        Args:
+            size (int): length of the square's sides
+
+        Raises:
+            TypeError: size argument isn't an integer
+            ValueError: size argument is negative
+
+        """
+
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
--- 0x06-python-classes/README.md
+++ 0x06-python-classes/README.md
@@ -5,3 +5,6 @@ Creating an empty class.
 
 ## Square with Size
 Adding a private instance attribute to the class.
+
+## Size Validation
+Validating the optional size argument.
