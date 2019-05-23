b645f149cc3bbdc4a6123f4d8694a908e737f006
HermesBoots <samuel.of.hermes@gmail.com>
2019-05-23 17:22:23 +0000

    Add third task: print a name

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Function to print a name"""
+
+
+def say_my_name(first_name, last_name=""):
+    """Print a full name
+
+    Args:
+        first_name (str): part of the name printed first
+        last_name (str): part of the name printed last
+
+    """
+
+    if not isinstance(first_name, str):
+        raise TypeError('first_name must be a string')
+    if not isinstance(last_name, str):
+        raise TypeError('last_name must be a string')
+    print('My name is {:s} {:s}'.format(first_name, last_name))
--- 0x07-python-test_driven_development/README.md
+++ 0x07-python-test_driven_development/README.md
@@ -5,3 +5,6 @@ Adding two numbers and returning the integer result.
 
 ## Divide a Matrix
 Dividing all numbers in a matrix by a given number.
+
+## Say My Name
+Printing a first name with default last name.
