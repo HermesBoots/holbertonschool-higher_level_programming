e889572406748b841dafda2e1fba7a49b372d3f4
HermesBoots <samuel.of.hermes@gmail.com>
2019-06-10 21:33:52 +0000

    Add attribute validation tests for rectangle

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -18,9 +18,57 @@ class RectangleTests (unittest.TestCase):
 
         importlib.reload(models.rectangle)
 
+    def test_AttributeValidation(self):
+        """Pass various values to attributes that validate input"""
+
+        r = Rectangle(1000, 999, 2000000, 1)
+        attrs = ('width', 'height', 'x', 'y')
+        good = [1, 2, 3, 4]
+        for index, name in enumerate(attrs):
+            bad = good.copy()
+            bad[index] = '10'
+            message = '{} must be an integer'.format(name)
+            with self.subTest(name=name, msg='init(\'10\')'):
+                with self.assertRaises(TypeError, msg=message):
+                    Rectangle(*bad)
+            with self.subTest(name=name, msg='r.name = \'10\''):
+                with self.assertRaises(TypeError, msg=message):
+                    setattr(r, name, '10')
+            bad[index] = [10]
+            with self.subTest(name=name, msg='init([10])'):
+                with self.assertRaises(TypeError, msg=message):
+                    Rectangle(*bad)
+            with self.subTest(name=name, msg='r.name = [10]'):
+                with self.assertRaises(TypeError, msg=message):
+                    setattr(r, name, [10])
+            bad[index] = -1
+            message = '{} must be > 0'.format(name)
+            with self.subTest(name=name, msg='init(-1)'):
+                with self.assertRaises(ValueError, msg=message):
+                    Rectangle(*bad)
+            with self.subTest(name=name, msg='r.name = -1'):
+                with self.assertRaises(ValueError, msg=message):
+                    setattr(r, name, -1)
+            if name not in 'xy':
+                bad[index] = 0
+                with self.subTest(name=name, msg='init(0)'):
+                    with self.assertRaises(ValueError, msg=message):
+                        Rectangle(*bad)
+                with self.subTest(name=name, msg='r.name = 0'):
+                    with self.assertRaises(ValueError, msg=message):
+                        setattr(r, name, 0)
+
     def test_InitTooFewArgs(self):
         """Giving the constructor too few arguments"""
 
         message = "__init__() missing 1 required positional argument: 'height'"
         with self.assertRaises(TypeError, msg=message):
             Rectangle(1)
+
+    def test_InitTooManyArgs(self):
+        """Giving the constructor too many arguments"""
+
+        message = '__init__() takes from 3 to 6 positional arguments but 7 ' \
+                  'were given'
+        with self.assertRaises(TypeError, msg=message):
+            Rectangle(1, 2, 3, 4, 5, 6)
