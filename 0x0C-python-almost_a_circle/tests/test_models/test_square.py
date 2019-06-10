77f06c0f229a481d5ff5d6c24ac547f0150d97a5
HermesBoots <samuel.of.hermes@gmail.com>
2019-06-10 20:57:56 +0000

    Fix assertEqual calls in square tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -7,6 +7,9 @@ import models.square
 import unittest
 
 
+Square = models.square.Square
+
+
 class SquareTests (unittest.TestCase):
     """Tests for the square object model"""
 
@@ -60,42 +63,42 @@ class SquareTests (unittest.TestCase):
         """Changing the size and seeing if it changes with and height"""
 
         s = Square(3)
-        assertEqual(s.width, 3)
-        assertEqual(s.height, 3)
+        self.assertEqual(s.width, 3)
+        self.assertEqual(s.height, 3)
         s.size = 100
-        assertEqual(s.width, 100)
-        assertEqual(s.height, 100)
+        self.assertEqual(s.width, 100)
+        self.assertEqual(s.height, 100)
 
     def test_ToDictionary(self):
         """Converting the object to a dictionary"""
 
         s = Square(3, 10, 4, 'square')
         d = {'id': 'square', 'size': 3, 'x': 10, 'y': 4}
-        assertEqual(s.to_dictionary(), d)
+        self.assertEqual(s.to_dictionary(), d)
 
     def test_UpdateAttributes(self):
         """Using the update method to change some of the object's attributes"""
 
         s = Square(3, 10, 4, 'square')
         s.update('changed')
-        assertEqual(s.id, 'changed')
+        self.assertEqual(s.id, 'changed')
         s.update('back', 5)
-        assertEqual(s.id, 'back')
-        assertEqual(s.size, 5)
+        self.assertEqual(s.id, 'back')
+        self.assertEqual(s.size, 5)
         s.update('another', 20, size=30)
-        assertEqual(s.size, 20)
+        self.assertEqual(s.size, 20)
         s.update('id', 1, 2, 3)
-        assertEqual(s.x, 2)
-        assertEqual(s.y, 3)
+        self.assertEqual(s.x, 2)
+        self.assertEqual(s.y, 3)
         s.update(y=50)
-        assertEqual(s.y, 50)
+        self.assertEqual(s.y, 50)
         s.update(id='id2', size=15, x=0, y=1)
-        assertEqual(s.id, 'id2')
-        assertEqual(s.size, 15)
-        assertEqual(s.x, 0)
-        assertEqual(s.y, 1)
+        self.assertEqual(s.id, 'id2')
+        self.assertEqual(s.size, 15)
+        self.assertEqual(s.x, 0)
+        self.assertEqual(s.y, 1)
         s.update(size=2, y=0, x=10, id=50)
-        assertEqual(s.id, 50)
-        assertEqual(s.size, 2)
-        assertEqual(s.x, 10)
-        assertEqual(s.y, 0)
+        self.assertEqual(s.id, 50)
+        self.assertEqual(s.size, 2)
+        self.assertEqual(s.x, 10)
+        self.assertEqual(s.y, 0)
