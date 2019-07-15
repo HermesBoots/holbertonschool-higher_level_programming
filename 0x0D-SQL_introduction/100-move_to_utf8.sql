bada56b6ff0e1de4895a623b1899d3e1750f3222
HermesBoots <samuel.of.hermes@gmail.com>
2019-07-15 18:12:18 +0000

    Add collation to columns in eighteenth task

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -5,8 +5,5 @@ ALTER DATABASE CHARACTER SET utf8mb4;
 ALTER DATABASE COLLATE utf8mb4_unicode_ci;
 
 -- change the table default
-ALTER TABLE first_table CHARACTER SET utf8mb4;
-ALTER TABLE first_table COLLATE utf8mb4_unicode_ci;
-
--- convert the existing string column
-ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4;
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
