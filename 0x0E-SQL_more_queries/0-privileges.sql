5487bdc3a353c0abdecea1d836806934ab6d60b7
HermesBoots <samuel.of.hermes@gmail.com>
2019-07-16 06:28:54 +0000

    Add first task: see privileges granted to 2 users

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,5 @@
+-- listing the privileges granted to 2 users
+-- first user
+SHOW GRANTS FOR user_0d_1@localhost;
+-- second user
+SHOW GRANTS FOR user_0d_2@localhost;
---
+++ 0x0E-SQL_more_queries/README.md
@@ -0,0 +1,4 @@
+# SQL - More Queries
+
+## My Privileges!
+Listing the privileges granted to 2 users.
