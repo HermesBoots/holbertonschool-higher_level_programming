69f0f562099423e5c71cb84385baf83f2a9fc19b
HermesBoots <701@holbertonschool.com>
2019-08-10 20:01:52 +0000

    Add updated data models with ORM relationships

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -4,6 +4,7 @@
 
 import sqlalchemy
 import sqlalchemy.ext.declarative
+import sqlalchemy.orm
 
 
 Base = sqlalchemy.ext.declarative.declarative_base()
@@ -13,6 +14,7 @@ class State (Base):
     """A U.S. state stored in a database
 
     Attributes:
+        cities (List[City]): list of cities in this state
         id (int): a unique ID for this record
         name (str): name of the state
 
@@ -21,3 +23,8 @@ class State (Base):
     __tablename__ = 'states'
     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
     name = sqlalchemy.Column(sqlalchemy.String(256), nullable=False)
+    cities = sqlalchemy.orm.relationship(
+        'City',
+        back_populates='state',
+        cascade='delete'
+    )
