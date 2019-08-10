12c8e2dd5a4811527cff2a1dfdf41c7a09006349
HermesBoots <701@holbertonschool.com>
2019-08-10 19:22:24 +0000

    Remove argument checking from task 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -9,8 +9,6 @@ import sys
 def main():
     """List the names and IDs of U.S. states in a database"""
 
-    if len(sys.argv) < 4:
-        sys.exit(1)
     connection = MySQLdb.connect(
         host='localhost',
         port=3306,
