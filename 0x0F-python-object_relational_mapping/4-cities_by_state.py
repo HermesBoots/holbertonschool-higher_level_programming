60461e54dc8c1b54e96ef4e778a736c6613040a0
HermesBoots <701@holbertonschool.com>
2019-08-10 00:40:18 +0000

    Add ordering to task 4

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -22,6 +22,7 @@ def main():
     cursor.execute('''
         SELECT `cities`.`id`, `cities`.`name`, `states`.`name` FROM `cities`
         INNER JOIN `states` ON `cities`.`state_id` = `states`.`id`
+        ORDER BY `cities`.`id`
     ''')
     records = cursor.fetchall()
     print('\n'.join(str(record) for record in sorted(records)))
