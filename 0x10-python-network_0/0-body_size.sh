a0ba5482fdf45b88354fdf7fced9f59d5cbf5e17
HermesBoots <701@holbertonschool.com>
2019-08-20 16:25:29 +0000

    Shorten task 0 to fit on 3 lines

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 # find the size in bytes of a web resource
-
-curl --silent --head "$@" \
-| grep --ignore-case --only-matching --perl-regexp '(?<=Content-Length:)\s+\d+' \
-| tr --delete ' '
+curl -Is "$@" | grep -Pio '(?<=Content-Length:)\s+\d+' | tr -d ' '
