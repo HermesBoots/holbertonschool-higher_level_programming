30fbd26c25ab94cd4ff949165e05f9289902ac43
HermesBoots <701@holbertonschool.com>
2019-08-26 16:41:57 +0000

    Change default value in task 1 hoping for hidden requirement

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -8,4 +8,4 @@ import urllib.request
 
 if __name__ == '__main__':
     with urllib.request.urlopen(sys.argv[1]) as response:
-        print(response.headers.get('X-Request-Id', ''))
+        print(response.headers.get('X-Request-Id'))
