87db004ff8eecc821e7a43bc05b95c23ddd31284
HermesBoots <701@holbertonschool.com>
2019-08-26 17:49:23 +0000

    Fix trailing parenthesis in task 4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -9,4 +9,4 @@ if __name__ == '__main__':
     with requests.get('https://intranet.hbtn.io/status') as response:
         print('Body response:')
         print('\t- type:', type(response.text))
-        print('\t- content:', response.text))
+        print('\t- content:', response.text)
