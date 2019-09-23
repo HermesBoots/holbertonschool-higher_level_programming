1326a13deec3f2e4d61a6c670851de4a6b2a5731
HermesBoots <701@holbertonschool.com>
2019-09-23 16:21:47 +0000

    Import Rectangle module in task 5 instead of copying it

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,36 +1,13 @@
 #!/usr/bin/node
 
-class Rectangle {
-  constructor (w, h) {
-    if (!(w > 0 && h > 0)) { return; }
-    this.width = w;
-    this.height = h;
+module.exports = class Square extends require('./4-rectangle.js') {
+  constructor (size) {
+    super(size, size);
   }
 
   charPrint (c) {
     if (c === undefined) { c = 'X'; }
     const row = c.repeat(this.width);
     for (let i = 0; i < this.height; i++) { console.log(row); }
-  }
-
-  double () {
-    this.width *= 2;
-    this.height *= 2;
-  }
-
-  print () {
-    this.charPrint('X');
-  }
-
-  rotate () {
-    const temp = this.width;
-    this.width = this.height;
-    this.height = temp;
-  }
-}
-
-module.exports = class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
   }
 };
